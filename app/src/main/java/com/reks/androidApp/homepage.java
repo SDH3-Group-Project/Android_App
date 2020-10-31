782f6df135d2c77ad6e1c5af95c239b60fb0efc6
Eoghan Spillane <eoghan.spillane@mycit.ie>
2020-10-31 19:16:24 +0000

    Created Sign in with google button and authenticated with Firebase

--- app/src/main/java/com/reks/androidApp/homepage.java
+++ app/src/main/java/com/reks/androidApp/homepage.java
@@ -2,14 +2,45 @@ package com.reks.androidApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class homepage extends AppCompatActivity {
+
+    TextView userName, userMail;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        logout = findViewById(R.id.logout);
+        userName = findViewById(R.id.userName);
+        userMail = findViewById(R.id.userMail);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            userName.setText(signInAccount.getDisplayName());
+            userMail.setText(signInAccount.getEmail());
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent signOut = new Intent(getApplicationContext(), Splashscreen.class);
+                startActivity(signOut);
+            }
+        });
+
     }
 
 }
