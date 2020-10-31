a906051a28c1583bacb207ab67495b2f3fee5319
Eoghan Spillane <eoghan.spillane@mycit.ie>
2020-10-31 18:12:21 +0000

    Created Splash Screen that redirects depending on Sign in Status

---
+++ app/src/main/java/com/reks/androidApp/Splashscreen.java
@@ -0,0 +1,42 @@
+package com.reks.androidApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+
+//      If the user is already logged in when you open the app they will be brought directly to the main menu, otherwise they will have to login
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                if(user != null){
+                    Intent homepageIntent = new Intent(getApplicationContext(), homepage.class);
+                    startActivity(homepageIntent);
+                }
+                else{
+                    Intent loginIntent = new Intent(getApplicationContext(), login.class);
+                    startActivity(loginIntent);
+                }
+            }
+        }, 2000);
+
+
+    }
+}
--- app/src/main/java/com/reks/androidApp/login.java
+++ app/src/main/java/com/reks/androidApp/login.java
@@ -2,12 +2,24 @@ package com.reks.androidApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
     private EditText lPasswordInput;
     private EditText lEmailInput;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -17,6 +29,15 @@ public class login extends AppCompatActivity {
 
         lEmailInput = findViewById(R.id.userEmailInput);
         lPasswordInput = findViewById(R.id.userPasswordInput);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
 }
