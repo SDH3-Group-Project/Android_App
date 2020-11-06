6061879c8d03a31e3bd1b6e8baa094c3866635f3
Kasparas <kasparasskruibis@gmail.com>
2020-11-06 14:57:25 +0000

    Added google maps api

--- app/src/main/java/com/reks/androidApp/MapsActivity.java
+++ app/src/main/java/com/reks/androidApp/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng seattle = new LatLng(47.606209, -122.332069);
+        mMap.addMarker(new MarkerOptions().position(seattle));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
     }
 }
