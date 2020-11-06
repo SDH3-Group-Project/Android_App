fe0919ff8c4c9b23d80d0310b098bf09bdce4ec6
Kasparas <kasparasskruibis@gmail.com>
2020-11-06 17:10:47 +0000

    Added ability to change location and get long and lat

--- app/src/main/java/com/reks/androidApp/MapsActivity.java
+++ app/src/main/java/com/reks/androidApp/MapsActivity.java
@@ -1,19 +1,21 @@
 package com.reks.androidApp;
 
 import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
-    private GoogleMap mMap;
+    private GoogleMap map;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +24,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Seattle and move the camera
         LatLng seattle = new LatLng(47.606209, -122.332069);
-        mMap.addMarker(new MarkerOptions().position(seattle));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
+        marker = map.addMarker(new MarkerOptions().position(seattle).draggable(true));
+        map.moveCamera(CameraUpdateFactory.newLatLng(seattle));
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                System.out.println(marker.getPosition().latitude);
+                System.out.println(marker.getPosition().longitude);
+            }
+        });
     }
+
+    public void getLongLat(View view) {
+        System.out.println(marker.getPosition().latitude);
+        System.out.println(marker.getPosition().longitude);
+    }
+
 }
