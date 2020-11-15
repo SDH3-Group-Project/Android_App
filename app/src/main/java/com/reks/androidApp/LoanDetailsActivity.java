b9d6dedaf46e997f634fedb067cfbd43ccd491b2
Roshan Sreekanth <roshan.sreekanth@mycit.ie>
2020-11-15 08:49:37 +0000

    Made loan details form and made it the main activity for testing

---
+++ app/src/main/java/com/reks/androidApp/LoanDetailsActivity.java
@@ -0,0 +1,74 @@
+package com.reks.androidApp;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class LoanDetailsActivity extends AppCompatActivity {
+
+    Spinner dependentsSpinner;
+    ArrayAdapter<CharSequence> dependentsAdapter;
+
+    Spinner propertySpinner;
+    ArrayAdapter<CharSequence> propertyAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle("Loan Details");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        dependentsSpinner = (Spinner) findViewById(R.id.dependents_spinner);
+        dependentsAdapter = ArrayAdapter.createFromResource(this, R.array.dependents_options, android.R.layout.simple_spinner_item);
+        dependentsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dependentsSpinner.setAdapter(dependentsAdapter);
+        dependentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getBaseContext(), adapterView.getItemAtPosition(i) + " is selected",Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        propertySpinner = (Spinner) findViewById(R.id.property_spinner);
+        propertyAdapter = ArrayAdapter.createFromResource(this, R.array.property_options, android.R.layout.simple_spinner_item);
+        propertyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        propertySpinner.setAdapter(propertyAdapter);
+        propertySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getBaseContext(), adapterView.getItemAtPosition(i) + " is selected",Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+}
