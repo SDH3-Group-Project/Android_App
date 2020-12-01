bb036006e7f43a033f8e90ff200c387bbc11f42e
Kasparas <kasparasskruibis@gmail.com>
2020-12-01 15:05:28 +0000

    Sent the review of the user to the database

--- app/src/main/java/com/reks/androidApp/feedback.java
+++ app/src/main/java/com/reks/androidApp/feedback.java
@@ -3,12 +3,48 @@ package com.reks.androidApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class feedback extends AppCompatActivity {
+
+    RadioGroup ratingSelector;
+
+    EditText review;
+
+    Button submit;
+
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        ratingSelector = findViewById(R.id.starRating);
+        review = findViewById(R.id.feedback);
+        submit = findViewById(R.id.feedbackSubmit);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int rating = ratingSelector.getCheckedRadioButtonId();
+                myRef.child("Reviews").child(user.getUid()).child("Rating").setValue(rating + 1);
+                myRef.child("Reviews").child(user.getUid()).child("Review").setValue(review.getText().toString());
+            }
+        });
     }
 }
