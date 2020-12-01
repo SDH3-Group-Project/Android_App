eada8685acda11f0de1bc244f3b79701ff6571e4
Eoghan Spillane <Eoghan.Spillane@mycit.ie>
2020-12-01 15:32:37 +0000

    Changed Star rating to actual stars

--- app/src/main/java/com/reks/androidApp/feedback.java
+++ app/src/main/java/com/reks/androidApp/feedback.java
@@ -2,12 +2,14 @@ package com.reks.androidApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -35,7 +37,7 @@ public class feedback extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        ratingSelector = findViewById(R.id.starRating);
+        //ratingSelector = findViewById(R.id.starRating);
         review = findViewById(R.id.feedback);
         submit = findViewById(R.id.feedbackSubmit);
         mAuth = FirebaseAuth.getInstance();
@@ -44,9 +46,18 @@ public class feedback extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Sending Review";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
                 rating = findViewById(ratingSelector.getCheckedRadioButtonId());
-                myRef.child("Reviews").child(user.getUid()).child("Rating").setValue(rating.getText().toString());
+               // myRef.child("Reviews").child(user.getUid()).child("Rating").setValue(rating.getText().toString());
                 myRef.child("Reviews").child(user.getUid()).child("Review").setValue(review.getText().toString());
+
+                review.setText("");
             }
         });
     }
